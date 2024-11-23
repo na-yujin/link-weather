ba34e0d9948c14060e3daae4402cccc61119fc0e
na-yujin <kfoyujin.gmail.com>
2024-11-23 23:45:28 +0900

    test

--- src/components/Layout/NavigationLayout/index.tsx
+++ src/components/Layout/NavigationLayout/index.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
 interface NavigationLayoutProps {
   children: ReactNode;
 }
 export default function NavigationLayout({children}:NavigationLayoutProps) {
   return (
-    <div className="bg-amber-200 flex grow h-full">
-      <div className="bg-amber-800">sidebar입니당</div>
+    <div className="bg-amber-200 flex h-[100vh]">
+      <Sidebar/>
       <div>{children}</div>
     </div>
   )
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+  return (
+    <div>
+      <div>사이드바 입니다용</div>
+      <div>weather</div>
+      <div>news</div>
+    </div>
+  )
+}
